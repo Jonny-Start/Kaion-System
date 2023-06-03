06eee9f3a215f44ca381d89a6f53c7de262feded
Jonny Alejandro Cano Acosta <jonnyalejandro.ca0910@gmail.com>
2023-06-03 11:25:03 -0500

    Datos quemados

--- src/containers/LandingPage/index.js
+++ src/containers/LandingPage/index.js
@@ -49,8 +49,9 @@ function LandingPage() {
             </h2>
             <p id="text">
               {" "}
-              Kaion System cuenta con las mejores habilidades para adaptarse a
-              tu equipo de trabajo.
+              Optimiza tu productividad con Kaion System la aplicación líder en
+              monitoreo y análisis del tiempo productivo para maximizar tus
+              resultados
             </p>
             <span id="contentButtonSlogan">
               <Button
@@ -68,55 +69,84 @@ function LandingPage() {
           <div id="contentDescription">
             <h3 id="titleFeature"> Características de KS</h3>
             <h2 id="subtitleFeature">
-              El tiempo es dinero y aquí te ayudamos a controlarlo.
+              Descubre cómo tus horas se traducen en resultados tangibles con
+              Kaion System, la solución inteligente para el monitoreo del tiempo
+              productivo
             </h2>
           </div>
           <div id="contentCardsFeatures">
             <div className="contentFeatures">
               <div className="feature">
-                <i className="fa-solid fa-stopwatch"></i>
+                <i class="fa-solid fa-clock"></i>
                 <span className="descriptionFeature">
-                  <h4 id="titleCard"> Monitoreo integral del tiempo productivo</h4>
-                  <p id="descriptionCard">Obtén una visión completa del tiempo dedicado a diferentes actividades, proyectos y tareas, en diferentes entornos de trabajo.</p>
+                  <h4 id="titleCard">
+                    {" "}
+                    Monitoreo integral del tiempo productivo
+                  </h4>
+                  <p id="descriptionCard">
+                    Obtén una visión completa del tiempo dedicado a diferentes
+                    actividades, proyectos y tareas, en diferentes entornos de
+                    trabajo.
+                  </p>
                 </span>
               </div>
               <div className="feature">
-                <i className="fa-solid fa-stopwatch"></i>
+                <i className="fa-solid fa-chart-line"></i>
                 <span className="descriptionFeature">
                   <h4 id="titleCard">Análisis en tiempo real</h4>
-                  <p id="descriptionCard">Accede a análisis y estadísticas en tiempo real para evaluar y optimizar la productividad individual de tus equipos de trabajo.</p>
+                  <p id="descriptionCard">
+                    Accede a análisis y estadísticas en tiempo real para evaluar
+                    y optimizar la productividad individual de tus equipos de
+                    trabajo.
+                  </p>
                 </span>
               </div>
             </div>
             <div className="contentFeatures">
               <div className="feature">
-                <i className="fa-solid fa-stopwatch"></i>
+                <i className="fa-solid fa-chart-pie"></i>
                 <span className="descriptionFeature">
                   <h4 id="titleCard">Visualización intuitiva</h4>
-                  <p id="descriptionCard">Utiliza gráficos claros y concisos para representar visualmente la distribución del tiempo productivo, permitiendo una fácil comprensión y toma de decisiones informadas.</p>
+                  <p id="descriptionCard">
+                    Utiliza gráficos claros y concisos para representar
+                    visualmente la distribución del tiempo productivo,
+                    permitiendo una fácil comprensión y toma de decisiones
+                    informadas.
+                  </p>
                 </span>
               </div>
               <div className="feature">
-                <i className="fa-solid fa-stopwatch"></i>
+                <i className="fa-solid fa-mobile"></i>
                 <span className="descriptionFeature">
                   <h4 id="titleCard"> Acceso multiplataforma</h4>
-                  <p id="descriptionCard">Disfruta de la flexibilidad de acceder a la aplicación desde dispositivos</p>
+                  <p id="descriptionCard">
+                    Disfruta de la flexibilidad de acceder a la aplicación desde
+                    dispositivos
+                  </p>
                 </span>
               </div>
             </div>
             <div className="contentFeatures">
               <div className="feature">
-                <i className="fa-solid fa-stopwatch"></i>
+                <i className="fa-solid fa-code-branch"></i>
                 <span className="descriptionFeature">
                   <h4 id="titleCard"> Integraciones flexibles</h4>
-                  <p id="descriptionCard">Conecta con otras herramientas y aplicaciones populares para importar datos y lograr una experiencia de seguimiento del tiempo más completa y automatizada.</p>
+                  <p id="descriptionCard">
+                    Conecta con otras herramientas y aplicaciones populares para
+                    importar datos y lograr una experiencia de seguimiento del
+                    tiempo más completa y automatizada.
+                  </p>
                 </span>
               </div>
               <div className="feature">
-                <i className="fa-solid fa-stopwatch"></i>
+                <i className="fa-solid fa-lock"></i>
                 <span className="descriptionFeature">
                   <h4 id="titleCard"> Seguridad y privacidad</h4>
-                  <p id="descriptionCard">Garantiza la seguridad de los datos y la privacidad de los usuarios mediante protocolos de seguridad robustos y prácticas de privacidad sólidas.</p>
+                  <p id="descriptionCard">
+                    Garantiza la seguridad de los datos y la privacidad de los
+                    usuarios mediante protocolos de seguridad robustos y
+                    prácticas de privacidad sólidas.
+                  </p>
                 </span>
               </div>
             </div>
@@ -133,25 +163,33 @@ function LandingPage() {
                 </span>
                 <span className="typePrice">
                   <h4>$</h4>
-                  <h2 className="h2price"> 20</h2>
-                  <h4>.000</h4>
+                  <h2 className="h2price"> 0</h2>
+                  <h4> /30 días</h4>
                 </span>
               </div>
               <div className="contentDescriptionCard">
                 <h2 className="h2Description"> Qué incluye?</h2>
                 <span className="descriptionPlan">
                   <p>
-                    {" "}
-                    <i className="fa-sharp fa-solid fa-list-check"></i> un
-                    proyecto
+                    <i className="fa-sharp fa-solid fa-list-check"></i> Registar
+                    1 proyecto
+                  </p>
+                  <p>
+                    <i className="fa-sharp fa-solid fa-thumbtack"></i> Agregar 5
+                    tareas
                   </p>
                   <p>
                     {" "}
-                    <i className="fa-solid fa-users"></i> 2 empleados
+                    <i className="fa-sharp fa-solid fa-user-tie"></i> Registrar
+                    un usuario administrador
                   </p>
                   <p>
-                    <i className="fa-solid fa-chart-simple"></i> No muestra
-                    gráficas
+                    {" "}
+                    <i className="fa-solid fa-users"></i> Registrar 2 empleados
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-chart-simple"></i> Muestra
+                    gráficas en tiempo real
                   </p>
                 </span>
               </div>
@@ -166,29 +204,37 @@ function LandingPage() {
             <div className="contentCardPrice">
               <div className="contentTitleCard">
                 <span className="typePlan">
-                  <h3 className="h3Plan"> ¡Gratis!</h3>
+                  <h3 className="h3Plan"> ¡Plus!</h3>
                 </span>
                 <span className="typePrice">
                   <h4>$</h4>
                   <h2 className="h2price"> 20</h2>
-                  <h4>.000</h4>
+                  <h4>.000 /mes</h4>
                 </span>
               </div>
               <div className="contentDescriptionCard">
                 <h2 className="h2Description"> Qué incluye?</h2>
                 <span className="descriptionPlan">
                   <p>
-                    {" "}
-                    <i className="fa-sharp fa-solid fa-list-check"></i> un
-                    proyecto
+                    <i className="fa-sharp fa-solid fa-list-check"></i> Registar
+                    3 proyectos
+                  </p>
+                  <p>
+                    <i className="fa-sharp fa-solid fa-thumbtack"></i> Agregar
+                    30 tareas
                   </p>
                   <p>
                     {" "}
-                    <i className="fa-solid fa-users"></i> 2 empleados
+                    <i className="fa-sharp fa-solid fa-user-tie"></i> Registrar
+                    dos usuarios administradores
                   </p>
                   <p>
-                    <i className="fa-solid fa-chart-simple"></i> No muestra
-                    gráficas
+                    {" "}
+                    <i className="fa-solid fa-users"></i> Registrar 5 empleados
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-chart-simple"></i> Muestra
+                    gráficas en dos semanas de uso
                   </p>
                 </span>
               </div>
@@ -203,29 +249,37 @@ function LandingPage() {
             <div className="contentCardPrice">
               <div className="contentTitleCard">
                 <span className="typePlan">
-                  <h3 className="h3Plan"> ¡Gratis!</h3>
+                  <h3 className="h3Plan"> ¡Pro!</h3>
                 </span>
                 <span className="typePrice">
                   <h4>$</h4>
-                  <h2 className="h2price"> 20</h2>
-                  <h4>.000</h4>
+                  <h2 className="h2price"> 80</h2>
+                  <h4>.000 /mes</h4>
                 </span>
               </div>
               <div className="contentDescriptionCard">
                 <h2 className="h2Description"> Qué incluye?</h2>
                 <span className="descriptionPlan">
                   <p>
-                    {" "}
-                    <i className="fa-sharp fa-solid fa-list-check"></i> un
-                    proyecto
+                    <i className="fa-sharp fa-solid fa-list-check"></i> Registar
+                    proyectos ilimitados
+                  </p>
+                  <p>
+                    <i className="fa-sharp fa-solid fa-thumbtack"></i> Agregar
+                    tareas ilimitadas
                   </p>
                   <p>
                     {" "}
-                    <i className="fa-solid fa-users"></i> 2 empleados
+                    <i className="fa-sharp fa-solid fa-user-tie"></i> Registrar
+                    5 usuarios administradores
                   </p>
                   <p>
-                    <i className="fa-solid fa-chart-simple"></i> No muestra
-                    gráficas
+                    {" "}
+                    <i className="fa-solid fa-users"></i> Registrar 30 empleados
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-chart-simple"></i> Muestra
+                    gráficas en tiempo real
                   </p>
                 </span>
               </div>
@@ -244,7 +298,9 @@ function LandingPage() {
             <span id="descriptionFooter">
               <h4 id="h4DescriptionFooter">
                 {" "}
-                Contando las horas de trabajo para alcanzar el éxito.{" "}
+                Kaion System: tu aliado para la productividad efectiva. Controla
+                tu tiempo, optimiza tu trabajo y alcanza tus objetivos con
+                facilidad.{" "}
               </h4>
             </span>
             <span id="titleFooter">
